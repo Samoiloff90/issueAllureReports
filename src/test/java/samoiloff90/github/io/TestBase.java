dce3e7cda1af2e9a138a861cd36827cd09dd1306
Eugene Samoilov <e.e.samoilov@mail.ru>
2022-03-01 21:21:33 +0300

    removed unnecessary dependencies

--- src/test/java/samoiloff90/github/io/TestBase.java
+++ src/test/java/samoiloff90/github/io/TestBase.java
@@ -2,7 +2,6 @@ package samoiloff90.github.io;
 
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.Configuration.*;
