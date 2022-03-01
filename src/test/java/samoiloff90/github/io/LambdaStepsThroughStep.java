d0623fd2d3ddb17ff30f492170942e4c6305c539
Eugene Samoilov <e.e.samoilov@mail.ru>
2022-03-01 21:24:54 +0300

    Add steps to test for Lambda

--- src/test/java/samoiloff90/github/io/LambdaStepsThroughStep.java
+++ src/test/java/samoiloff90/github/io/LambdaStepsThroughStep.java
@@ -1,6 +1,20 @@
 package samoiloff90.github.io;
 
-public class LambdaStepsThroughStep {
+import org.junit.jupiter.api.Test;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static io.qameta.allure.Allure.step;
+import static samoiloff90.github.io.TestData.urlRepo;
 
-
+public class LambdaStepsThroughStep extends TestBase{
+    @Test
+    public void testLambdaSteps() {
+        step ("Ищем репозиторий " + urlRepo, () -> {
+            $(".header-search-input").setValue(urlRepo).pressEnter();
+            $("a[href*='Samoiloff90/junitForDns']").click();
+        });
+        step("Проверяем, что существует Issue", () -> {
+            $("[data-content='Issues']").shouldHave(text("Issues"));
+        });
+    }
 }
