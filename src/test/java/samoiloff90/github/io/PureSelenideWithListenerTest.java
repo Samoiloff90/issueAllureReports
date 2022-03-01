d65969a3a6eba5135da72678af7fcdeb76b31dd2
Eugene Samoilov <e.e.samoilov@mail.ru>
2022-03-01 20:29:23 +0300

    Replacing set value

--- src/test/java/samoiloff90/github/io/PureSelenideWithListenerTest.java
+++ src/test/java/samoiloff90/github/io/PureSelenideWithListenerTest.java
@@ -2,13 +2,14 @@ package samoiloff90.github.io;
 import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
+import static samoiloff90.github.io.TestData.urlRepo;
 
 
 public class PureSelenideWithListenerTest extends TestBase{
 
     @Test
     public void testIssueSearch() {
-        $(".header-search-input").setValue("Samoiloff90/junitForDns").pressEnter();
+        $(".header-search-input").setValue(urlRepo).pressEnter();
         $("a[href*='Samoiloff90/junitForDns']").click();
         $("[data-content='Issues']").shouldHave(text("Issues"));
     }
