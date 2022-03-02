5ec685faeefb790a3f4d559f0a2404712adbb528
Eugene Samoilov <e.e.samoilov@mail.ru>
2022-03-02 21:06:10 +0300

    added searchTeam

--- src/test/java/samoiloff90/github/io/PureSelenideWithListenerTest.java
+++ src/test/java/samoiloff90/github/io/PureSelenideWithListenerTest.java
@@ -2,6 +2,7 @@ package samoiloff90.github.io;
 import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$x;
 import static samoiloff90.github.io.TestData.urlRepo;
 
 public class PureSelenideWithListenerTest extends TestBase{
@@ -12,4 +13,12 @@ public class PureSelenideWithListenerTest extends TestBase{
         $("a[href*='Samoiloff90/junitForDns']").click();
         $("[data-content='Issues']").shouldHave(text("Issues"));
     }
+
+    @Test
+    public void searchTeam() {
+        $x("//a[text()='Team']").click();
+        $x("//h1[text()='Build like the best teams on the planet']").shouldHave(text("Build like the best teams on the planet"));
+    }
+
+
 }
